96c6ef679f26d70d7ce2d595ff53805a4dce7796
Ryde <isaac.amancha@gmail.com>
2018-01-30 21:09:39 +0100

    Añadidos comentarios acerca de inicializacion de variables para guardar en la Branch(punto03) Punto 4

--- EjercicioPDF1/Form1.cs
+++ EjercicioPDF1/Form1.cs
@@ -13,6 +13,10 @@ namespace EjercicioPDF1
     public partial class Form1 : Form
     {
         Int32 i, j, aux;//prieba de comentario GitHub
+        //variables i y j se inicializan en los for por lo tanto aqui no le damos valor
+        //creamos la variable i para mover el array y coger la primera pos
+        //la variable j la usaremos apra mover el array e ir comparando la pos del array[i] con las pos del array[j]
+        //aux sera una variable auxiliar donde guardaremos un valor del array para luego guardarlo en la siguiente pos correspondiente
         public Form1()
         {
             InitializeComponent();
@@ -20,8 +24,8 @@ namespace EjercicioPDF1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Int32[] array = { 12, 18, 6, 42, 44, 52, 67, 94 };
-            printArray(lb_array, array);
+            Int32[] array = { 12, 18, 6, 42, 44, 52, 67, 94 };//creamos un array con los valores dados
+            printArray(lb_array, array);//llamaos a una funcion para imprimir el array
 
             Classificacio(array);
             printArray(lb_res, array);
