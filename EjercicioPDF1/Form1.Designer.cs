2bcfdbfcba62d4325a74b16f418bfef3bfce8c47
Ryde <isaac.amancha@gmail.com>
2018-01-30 21:36:25 +0100

    punto 06 prueba de cambio de nombres de variable aux->numAux y el metodo de ordenacion de Clasificacion->Ordenacion. Todo esto aplicado a la segunda rama que hemos creado

--- EjercicioPDF1/Form1.Designer.cs
+++ EjercicioPDF1/Form1.Designer.cs
@@ -72,7 +72,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(449, 250);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lb_res);
--- EjercicioPDF1/Form1.cs
+++ EjercicioPDF1/Form1.cs
@@ -12,11 +12,11 @@ namespace EjercicioPDF1
 {
     public partial class Form1 : Form
     {
-        Int32 i, j, aux;//prieba de comentario GitHub
+        Int32 i, j, numAux;//prieba de comentario GitHub
         //variables i y j se inicializan en los for por lo tanto aqui no le damos valor
         //creamos la variable i para mover el array y coger la primera pos
         //la variable j la usaremos apra mover el array e ir comparando la pos del array[i] con las pos del array[j]
-        //aux sera una variable auxiliar donde guardaremos un valor del array para luego guardarlo en la siguiente pos correspondiente
+        //numAux sera una variable numAuxiliar donde guardaremos un valor del array para luego guardarlo en la siguiente pos correspondiente
         public Form1()
         {
             InitializeComponent();
@@ -24,13 +24,17 @@ namespace EjercicioPDF1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Int32[] array = { 12, 18, 6, 42, 44, 52, 67, 94 };//creamos un array con los valores dados
+            //Clock inici= currentTime();
+            //Int32[] array = { 12, 18, 6, 42, 44, 52, 67, 94 };//creamos un array con los valores dados
+            Int32[] array = { 12, 18, 6, 42, 44 };//array con 5 valores
+            //Int32[] array = { 12, 18, 6, 42, 44, 52, 67, 94, 1, 2, 3 };//array con 10 valores
+            //Int32[] array = { 12, 18, 6, 42, 44, 52, 67, 94, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 543, 23, 54, 66, 12, 69, 7, 89, 23, 12 };//array con 20 valores
             printArray(lb_array, array);//llamaos a una funcion para imprimir el array
 
-            Classificacio(array);//llamamos al metodo que va a ordenar el array
+            Ordenacion(array);//llamamos al metodo que va a ordenar el array
             printArray(lb_res, array);
         }
-        private void Classificacio(Int32[] array)
+        private void Ordenacion(Int32[] array)
         {
             for (i = 0; i < array.Length; i++)
             {
@@ -38,9 +42,9 @@ namespace EjercicioPDF1
                 {
                     if (array[i] > array[j])
                     {
-                        aux = array[i];
+                        numAux = array[i];
                         array[i] = array[j];
-                        array[j] = aux;
+                        array[j] = numAux;
                     }
                 }
             }
