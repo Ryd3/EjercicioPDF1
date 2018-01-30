99652fba9f7f0f4cb91a9a626edaab1ce7e8e3bd
Ryde <isaac.amancha@gmail.com>
2018-01-30 20:50:55 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioPDF1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace EjercicioPDF1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_array = new System.Windows.Forms.Label();
+            this.lb_res = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_array
+            // 
+            this.lb_array.AutoSize = true;
+            this.lb_array.Location = new System.Drawing.Point(13, 51);
+            this.lb_array.Name = "lb_array";
+            this.lb_array.Size = new System.Drawing.Size(0, 17);
+            this.lb_array.TabIndex = 0;
+            // 
+            // lb_res
+            // 
+            this.lb_res.AutoSize = true;
+            this.lb_res.Location = new System.Drawing.Point(13, 143);
+            this.lb_res.Name = "lb_res";
+            this.lb_res.Size = new System.Drawing.Size(0, 17);
+            this.lb_res.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Array desordenado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Array ordenado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_res);
+            this.Controls.Add(this.lb_array);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_array;
+        private System.Windows.Forms.Label lb_res;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EjercicioPDF1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioPDF1
+{
+    public partial class Form1 : Form
+    {
+        Int32 i, j, aux;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Int32[] array = { 12, 18, 6, 42, 44, 52, 67, 94 };
+            printArray(lb_array, array);
+
+            Classificacio(array);
+            printArray(lb_res, array);
+        }
+        private void Classificacio(Int32[] array)
+        {
+            for (i = 0; i < array.Length; i++)
+            {
+                for (j = i; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        aux = array[i];
+                        array[i] = array[j];
+                        array[j] = aux;
+                    }
+                }
+            }
+        }
+        public void printArray(Label lb, Int32[] array)
+        {
+            for (i = 0; i < array.Length; i++)
+            {
+                lb.Text += array[i].ToString() + "  ";
+            }
+        }
+    }
+}
